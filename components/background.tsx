3a76b3c028c1ad06ad11ee7f535128d8c9dd7e01
Simon Portillo <simon.portillo@outlook.com>
2025-09-25 19:11:24 +0200

    background mask for text readability

--- components/background.tsx
+++ components/background.tsx
@@ -5,16 +5,19 @@ import PixelBlast from "@/components/PixelBlast";
 export function Background() {
   return (
     <div className="fixed inset-0 pointer-events-none z-0">
-      <PixelBlast
-        variant="circle"
-        pixelSize={8}
-        patternDensity={0.5}
-        speed={0.3}
-        transparent={true}
-        edgeFade={0.3}
-        autoPauseOffscreen={true}
-        color="#34d399"
-      />
+      {/* Main PixelBlast effect with text readability mask */}
+      <div className="absolute inset-0 bg-content-mask">
+        <PixelBlast
+          variant="circle"
+          pixelSize={8}
+          patternDensity={0.5}
+          speed={0.3}
+          transparent={true}
+          edgeFade={0.3}
+          autoPauseOffscreen={true}
+          color="#34d399"
+        />
+      </div>
     </div>
   );
 }
